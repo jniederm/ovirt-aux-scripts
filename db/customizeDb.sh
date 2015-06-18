cd68d4093da57f569e93945b7ceee200f50bc71b
Jakub Niedermertl <jniederm@redhat.com>
2015-06-18 11:19:41 +0200

    customizeDb.sh : variable rename

--- db/customizeDb.sh
+++ db/customizeDb.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 if [[ $# != 1 ]] ; then
-    echo 'Usage: cleanDb.sh <database-and-user-name>';
+    echo 'Usage: customizeDb.sh <database-name>';
     exit 1;
 fi
 
-DB_AND_USER_NAME=$1
+DB_NAME=$1
 
-echo Customizing database ${DB_AND_USER_NAME} 
-psql -U postgres -d ${DB_AND_USER_NAME} -c "UPDATE vdc_options set option_value = 'false' where option_name = 'SSLEnabled';" &&
-psql -U postgres -d ${DB_AND_USER_NAME} -c "UPDATE vdc_options set option_value = 'false' where option_name = 'UseSecureConnectionWithServers';" &&
-psql -U postgres -d ${DB_AND_USER_NAME} -c "UPDATE vdc_options set option_value = 'false' where option_name = 'EncryptHostCommunication';" &&
-psql -U postgres -d ${DB_AND_USER_NAME} -c "UPDATE vdc_options set option_value = 'false' where option_name = 'InstallVds';" &&
-echo database ${DB_AND_USER_NAME} successfully updated
+echo Customizing database ${DB_NAME} 
+psql -U postgres -d ${DB_NAME} -c "UPDATE vdc_options set option_value = 'false' where option_name = 'SSLEnabled';" &&
+psql -U postgres -d ${DB_NAME} -c "UPDATE vdc_options set option_value = 'false' where option_name = 'UseSecureConnectionWithServers';" &&
+psql -U postgres -d ${DB_NAME} -c "UPDATE vdc_options set option_value = 'false' where option_name = 'EncryptHostCommunication';" &&
+psql -U postgres -d ${DB_NAME} -c "UPDATE vdc_options set option_value = 'false' where option_name = 'InstallVds';" &&
+echo database ${DB_NAME} successfully updated
 
