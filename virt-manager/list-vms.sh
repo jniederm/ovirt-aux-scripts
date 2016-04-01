dc4be5831c1196f9a2b76292f0ac7862955a227a
Jakub Niedermertl <jniederm@redhat.com>
2016-04-01 16:31:26 +0200

    list-vms: multiple NICs support + refactoring

--- virt-manager/list-vms.sh
+++ virt-manager/list-vms.sh
@@ -1,22 +1,35 @@
 #!/usr/bin/env bash
 
-if [[ $EUID != 0 ]] ; then
-    echo "root permitions required";
-    exit 1
-fi
+# todo add args --serial-console/-c and --ssh/-s, list table, read number, connect/print error
 
-process_line() {
+process_virsh_list_line() {
     LINE=$1
     local ORIGINAL_IFS=IFS
     local IFS=' '
-    local MAC=$(echo $LINE | cut -f 2 -d ' ' | xargs sudo virsh dumpxml | grep '<mac ' | grep -oP '(\w{2}:){5}\w{2}')
-    local IP=$(echo $ARP | grep $MAC | cut -f 1 -d ' ')
-    IP=${IP:--}
-    IFS=ORIGINAL_IFS
-    echo -e $LINE \\t $IP
+    local MACS=$(echo $LINE | cut -f 2 -d ' ' | xargs sudo virsh dumpxml | grep '<mac ' | grep -oP '(\w{2}:){5}\w{2}')
+    echo -en "$LINE \\t"
+    while read MAC ; do
+        local IP=$(mac_to_ip "$MAC")
+        echo -n " $IP"
+    done <<< "$MACS"
+    echo
+}
+
+# in: mac string
+# out: either IP if found or '-'
+mac_to_ip() {
+    local MAC=$1
+    local IP=$(echo "$ARP" | grep $MAC | cut -f 1 -d ' ')
+    IP="${IP:--}"
+    echo $IP
 }
 
 RAW_LIST=$(virsh list --all | grep -P -- '(-|(\d+))\s+\S+\s+\S+')
+if [[ x"$RAW_LIST" == x && $EUID != 0 ]] ; then
+    echo '`virsh list --all` output no vms and script is not run as root. Try `sudo` or check out https://major.io/2015/04/11/run-virsh-and-access-libvirt-as-a-regular-user/';
+    exit 1
+fi
+
 ARP=$(arp)
 
 ORIGINAL_IFS=$IFS
@@ -24,7 +37,7 @@ IFS='
 '
 for LINE in $RAW_LIST
 do
-    process_line $LINE
+    process_virsh_list_line $LINE
 done
 IFS=$ORIGINAL_IFS
 
