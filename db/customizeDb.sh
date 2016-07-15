bb629c4e0d9d0ba4792fea822ccbea23f4b9d82b
Jakub Niedermertl <jniederm@redhat.com>
2016-07-15 13:54:37 +0200

    db: removed unused 'UseSecureConnectionWithServers' option

--- db/customizeDb.sh
+++ db/customizeDb.sh
@@ -7,10 +7,8 @@ fi
 
 DB_NAME=$1
 
-echo Customizing database ${DB_NAME} 
+echo Customizing database ${DB_NAME}
 psql -U postgres -d ${DB_NAME} -c "UPDATE vdc_options set option_value = 'false' where option_name = 'SSLEnabled';" &&
-psql -U postgres -d ${DB_NAME} -c "UPDATE vdc_options set option_value = 'false' where option_name = 'UseSecureConnectionWithServers';" &&
 psql -U postgres -d ${DB_NAME} -c "UPDATE vdc_options set option_value = 'false' where option_name = 'EncryptHostCommunication';" &&
 psql -U postgres -d ${DB_NAME} -c "UPDATE vdc_options set option_value = 'false' where option_name = 'InstallVds';" &&
 echo database ${DB_NAME} successfully updated
-
