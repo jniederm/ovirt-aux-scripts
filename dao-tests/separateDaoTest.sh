85f37cbe51c1c7933fcbd9b7a842fd9e22ad6df6
Jakub Niedermertl <jniederm@redhat.com>
2016-09-13 19:44:53 +0200

    rename ov-dao-test -> run-dao-tests.sh

--- dao-tests/separateDaoTest.sh
+++ dao-tests/separateDaoTest.sh
@@ -10,5 +10,5 @@ TEST_CLASS_NAME=$1
 DIR=$(dirname ${BASH_SOURCE[0]});
 
 $DIR/../db/cleanDb.sh engine_dao_tests engine &&
-PGPASSWORD=engine ./packaging/dbscripts/schema.sh -c apply -d engine_dao_tests -u engine &&
+PGPASSWORD=a ./packaging/dbscripts/schema.sh -c apply -d engine_dao_tests -u engine &&
 mvn -f ./backend/manager/modules/dal -Dtest=${TEST_CLASS_NAME} test
