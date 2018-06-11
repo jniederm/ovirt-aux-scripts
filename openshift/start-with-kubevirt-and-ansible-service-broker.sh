2274e9d6ef362b6e48ce6bae22a7cc61535b0b83
Jakub Niedermertl <jniederm@redhat.com>
2018-06-11 15:13:39 +0200

    start script

--- openshift/start-with-kubevirt-and-ansible-service-broker.sh
+++ openshift/start-with-kubevirt-and-ansible-service-broker.sh
@@ -17,18 +17,19 @@ oc adm policy add-scc-to-user privileged system:serviceaccount:kube-system:kubev
 #docker pull docker.io/ansibleplaybookbundle/kubevirt-apb
 #docker run --rm --net=host -v $HOME/.kube:/opt/apb/.kube:z -u $UID docker.io/ansibleplaybookbundle/kubevirt-apb provision --extra-vars 'namespace=kube-system' --extra-vars 'admin_user=admin' --extra-vars 'admin_password=admin' --extra-vars 'namespace=kube-system' --extra-vars 'cluster=openshift' --extra-vars 'tag=v0.4.1'
 
-kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/v0.4.1/kubevirt.yaml
+# kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/v0.4.1/kubevirt.yaml
+kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/v0.5.0/kubevirt.yaml
 
 #oc login -u admin
 
-echo ***** before
+echo '*****' before
 apb setup | true
-echo ***** after
+echo '*****' after
 
 oc new-project project1
 oc project project1
-oc create -f $CURRENT_DIR/blank-ovm.yml
-oc create -f $CURRENT_DIR/david-ovm.yml
+#oc create -f $CURRENT_DIR/blank-ovm.yml
+#oc create -f $CURRENT_DIR/david-ovm.yml
 
 
 # oc create -f blank-ovm.yml
